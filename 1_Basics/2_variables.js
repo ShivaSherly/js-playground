84efb4d5887abc0d4e15838eebe6b41fc06764ff
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-03-23 14:40:27 +0530

    base code added
    base code added

---
+++ 1_Basics/2_variables.js
@@ -0,0 +1,16 @@
+// String
+let myname="shiva";
+console.log(myname);
+
+// Char
+let mychar='I';
+
+// Array
+let myarray=[0,1,24,423,534,23242,5534];
+console.log(myarray);
+
+// Number (int)
+let mynumber=5;
+console.log(mynumber);
+let mysecondNumber=23;
+console.log(mysecondNumber);
---
+++ 3_Advanced/1_ES6_sample.js
@@ -0,0 +1,5 @@
+let mylist=[4,5,3,5];
+
+mylist.map((x)=>{
+    console.log(x);
+})
