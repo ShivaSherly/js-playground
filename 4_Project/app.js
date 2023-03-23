9998df707d5dfae0df47f723f649494a0bfd4a82
ShivaSherly <117709004+ShivaSherly@users.noreply.github.com>
2023-03-23 15:30:53 +0530

    new project started

---
+++ 4_Project/app.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+
+console.log(chalk.blue.bgWhite('Hello world!'));
+console.log(chalk.green("my success messsage"))
+console.log(chalk.red("my getting error"))
+console.log(chalk.grey("my intial warning!"))
+console.log(chalk.cyan("my brief info"))
