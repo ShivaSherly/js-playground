57ca54cd593eef249151bd93f976ecdb75c80761
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-05-02 12:13:05 +0530

    delted
    deleted

--- 3_Advanced/1_ES6_sample.js
+++
@@ -1,5 +0,0 @@
-let mylist=[4,5,3,5];
-
-mylist.map((x)=>{
-    console.log(x);
-})
--- 4_Project/app.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-const errorMessage=require('./utils/errorMessage');
-
-
-errorMessage.displayError("Unable to place swggy order please try after sometime..!");
-errorMessage.displayWarning("Please enter valid email address");
-errorMessage.displayInfo("Your order placed successfully");
